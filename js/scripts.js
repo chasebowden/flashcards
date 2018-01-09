ff7f998705455d2fb88265dc0b6398dc3ef4ebbe
Alex Ficklin and Chase Bowden <chase@>
2018-01-09 11:25:26 -0800

    Add CSS, JS and HTML to Flashcard Website.

---
+++ js/scripts.js
@@ -0,0 +1,135 @@
+$(document).ready(function() {
+  $("#javascript").click(function() {
+    //$(".hidden").removeClass();
+    $("#javascript span").toggleClass("show");
+    // $(".show").toggleClass("hidden");
+
+  });
+
+  $("#operators").click(function() {
+    //$(".hidden").removeClass();
+    $("#operators span").toggleClass("show");
+    // $(".show").toggleClass("hidden");
+
+  });
+
+  $("#variables").click(function() {
+    //$(".hidden").removeClass();
+    $("#variables span").toggleClass("show");
+    // $(".show").toggleClass("hidden");
+
+  });
+
+  $("#variableNaming").click(function() {
+    //$(".hidden").removeClass();
+    $("#variableNaming span").toggleClass("show");
+    // $(".show").toggleClass("hidden");
+
+  });
+
+  $("#functions").click(function() {
+      //$(".hidden").removeClass();
+    $("#functions span").toggleClass("show");
+      // $(".show").toggleClass("hidden");
+
+  });
+
+  $("#methods").click(function() {
+        //$(".hidden").removeClass();
+    $("#methods span").toggleClass("show");
+        // $(".show").toggleClass("hidden");
+
+  });
+
+  $("#arguments").click(function() {
+        //$(".hidden").removeClass();
+    $("#arguments span").toggleClass("show");
+        // $(".show").toggleClass("hidden");
+
+  });
+
+  $("#parameters").click(function() {
+        //$(".hidden").removeClass();
+    $("#parameters span").toggleClass("show");
+        // $(".show").toggleClass("hidden");
+
+  });
+
+  $("#return").click(function() {
+        //$(".hidden").removeClass();
+    $("#return span").toggleClass("show");
+        // $(".show").toggleClass("hidden");
+
+  });
+
+  $("#chainingMethods").click(function() {
+        //$(".hidden").removeClass();
+    $("#chainingMethods span").toggleClass("show");
+        // $(".show").toggleClass("hidden");
+
+  });
+
+  $("#strings").click(function() {
+        //$(".hidden").removeClass();
+    $("#strings span").toggleClass("show");
+        // $(".show").toggleClass("hidden");
+
+  });
+
+  $("#booleans").click(function() {
+        //$(".hidden").removeClass();
+    $("#booleans span").toggleClass("show");
+        // $(".show").toggleClass("hidden");
+
+  });
+
+  $("#undefined").click(function() {
+        //$(".hidden").removeClass();
+    $("#undefined span").toggleClass("show");
+        // $(".show").toggleClass("hidden");
+
+  });
+  $("#nan").click(function() {
+        //$(".hidden").removeClass();
+    $("#nan span").toggleClass("show");
+        // $(".show").toggleClass("hidden");
+
+  });
+  $("#escape").click(function() {
+        //$(".hidden").removeClass();
+    $("#escape span").toggleClass("show");
+        // $(".show").toggleClass("hidden");
+
+  });
+  $("#alert").click(function() {
+        //$(".hidden").removeClass();
+    $("#alert span").toggleClass("show");
+        // $(".show").toggleClass("hidden");
+
+  });
+  $("#comments").click(function() {
+        //$(".hidden").removeClass();
+    $("#comments span").toggleClass("show");
+        // $(".show").toggleClass("hidden");
+
+  });
+  $("#jQuery").click(function() {
+        //$(".hidden").removeClass();
+    $("#jQuery span").toggleClass("show");
+        // $(".show").toggleClass("hidden");
+
+  });
+  $("#attributes").click(function() {
+        //$(".hidden").removeClass();
+    $("#attributes span").toggleClass("show");
+        // $(".show").toggleClass("hidden");
+
+  });
+  $("#gitHub").click(function() {
+        //$(".hidden").removeClass();
+    $("#gitHub span").toggleClass("show");
+        // $(".show").toggleClass("hidden");
+
+  });
+
+});
